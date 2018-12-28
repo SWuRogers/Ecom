a8620c301e8e840b1ca0eec933f15d2150be016d
Sam Wu <swu@fct.ca>
2018-12-28 13:32:30 -0500

    Initial check in.
    Signed-off-by: Sam Wu <swu@fct.ca>

---
+++ ECom.Categlog/ServiceFabricCatelogRepository.cs
@@ -0,0 +1,27 @@
+﻿using ECom.Catelog.Model;
+using Microsoft.ServiceFabric.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECom.Categlog
+{
+    public class ServiceFabricCatelogRepository : ICatelogRepository
+    {
+        private IReliableStateManager _stateManager;
+        public ServiceFabricCatelogRepository(IReliableStateManager stateManager)
+        {
+            this._stateManager = stateManager;
+        }
+        public async Task AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ECom.Catelog.Model/ICatelogRepository.cs
@@ -0,0 +1,14 @@
+﻿using ECom.Catelog.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECom.Catelog.Model
+{
+    public interface ICatelogRepository
+    {
+        Task<IEnumerable<Product>> GetAllProducts();
+        Task AddProduct(Product product);
+    }
+}
